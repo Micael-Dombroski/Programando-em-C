8eb74ba9b9abef5402749871db8be377739becf5
Dust C <137210879+Micael-Dombroski@users.noreply.github.com>
2025-08-02 20:22:10 -0300

    more recursive functions and procedures

--- estudos/functions-procedures/recursion.c
+++ estudos/functions-procedures/recursion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 //recursive procedure that prints numbers from N until 0
 void printUntil0(int n) {
@@ -28,19 +29,98 @@ int nthNumberFromFibonacciSequence(int n) {
     else {
         if(n == 2)
             return 1;
-        else {
+        else
             return nthNumberFromFibonacciSequence(n-1) + nthNumberFromFibonacciSequence(n - 2);
-        }
+    }
+}
+//fibonacci with recursion is not efficient, to get the 6th number the 
+//function must be called 14 times after the call in main
+int xToPowerOfN(int x, int n) {
+    if (n == 0) return 1;
+    return x * xToPowerOfN(x, n - 1);
+}
+int sumFrom1ToN(int n) {
+    if(n == 0) return 0;
+    else {
+        if(n == 1) return 1;
+        return n + sumFrom1ToN(n - 1);
+    }
+}
+int sumOfTheNumbersInArray(int nums[], int index) {
+    if(index - 1 == 0) return nums[0];
+    else {
+        return nums[index - 1] + sumOfTheNumbersInArray(nums, index - 1);
+    }
+}
+int greaterValueInTheArray(int nums[], int index, int gIndex) {
+    if(index == 0) return nums[gIndex];
+    else {
+        if(nums[index - 1] > nums[gIndex])
+            return greaterValueInTheArray(nums, index - 1, index - 1);
+        else
+            return greaterValueInTheArray(nums, index - 1, gIndex);
+    }
+}
+void reverseArray(int arr[], int start, int end) {
+    if(start >= end) return;
+    int copy = arr[end-1];
+    arr[end-1] = arr[start];
+    arr[start] = copy;
+    reverseArray(arr, start + 1, end - 1);
+}
+void printArray(int arr[], int size) {
+    if(size == 1)
+        printf("%d ", arr[size - 1]);
+    else {
+        printf("%d ", arr[size - 1]);
+        printArray(arr, size - 1);
+    }
+}
+void printReverseArray(int arr[], int size) {
+    if(size <= 0)
+        return;
+    else {
+        printReverseArray(arr, size - 1);
+        printf("%d ", arr[size - 1]);
     }
 }
 int main(void) {
-    int n;
-    printf("Write a number: \n");
-    scanf("%d", &n);
-    printUntil0(n);
+    int n1, n2;
+    printf("Write two integers positive numbers: \n");
+    scanf("%d%d", &n1, &n2);
+    printUntil0(n1);
     printf("\n");
-    printUntilN(n);
-    printf("\nFactorial of %d: %d\n", n, factorial(n));
-    printf("The elemente number %d from Fibonacci sequence is: %d\n", n, nthNumberFromFibonacciSequence(n));
+    printUntilN(n1);
+    printf("\nFactorial of %d: %d\n", n1, factorial(n1));
+    if (n1 > 0) {
+        printf("The elemente number %d from Fibonacci sequence is: %d\n", n1, nthNumberFromFibonacciSequence(n1));
+        printf("%d to power of %d is: %d\n", n1, n2, xToPowerOfN(n1, n2));
+    }
+    printf("Sum from 1 to %d: %d\n", n1, sumFrom1ToN(n1));
+    int size;
+    printf("Write the size of the array: \n");
+    scanf("%d", &size);
+    int nums[size];
+    if(size > 0) {
+        for(int i = 0; i < size; i++) {
+            int ret = 0;
+            do {
+                printf("Write the number in position %d: \n", i);
+                ret = scanf("%d", &nums[i]);
+            } while (ret != 1);
+        }
+        printf("Sum of the numbers ");
+        printArray(nums, size);
+        printf("is: %d\n", sumOfTheNumbersInArray(nums, size));
+        printf("The greater value in the array is: %d\n", greaterValueInTheArray(nums, size, 0));
+        printArray(nums, size);
+        printf("\n");
+        reverseArray(nums, 0, size);
+        printArray(nums, size);
+        printf("\n");
+        printReverseArray(nums, size);
+        printf("\n");
+    }
+    
     return 0;
 }
